99bebb531cb7f0c724ee1d25fac04066d1ff4098
ctninman <89664157+ctninman@users.noreply.github.com>
2022-01-25 12:00:42 -0500

    commit tree e6a3645d1574632da454e310c8dbae3a324dc279

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,16 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev, item_name, value)
+    new_freebie = Freebie.create(item_name: item_name, value: value, dev: dev, company: self)
+  end
+
+  def self.oldest_company
+    self.order(:founding_year).first
+  end
+
+  def self.youngest_company
+    self.order(:founding_year).last
+  end
 end
